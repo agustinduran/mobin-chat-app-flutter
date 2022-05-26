aed877898f790eb38737a22afd2c72d11f4cad55
Agustín Durán <agustineduran@gmail.com>
2022-05-26 02:54:27 -0300

    do login

--- lib/app/presentation/pages/login/login_controller.dart
+++ lib/app/presentation/pages/login/login_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:mobin_app/app/domain/entities/login_response.dart';
+import 'package:mobin_app/app/domain/services/user_service.dart';
 
 class LoginController extends GetxController {
 
@@ -8,21 +10,22 @@ class LoginController extends GetxController {
 
   RxBool hidePassword = true.obs;
 
+  UserService service = UserService();
+
   final formKeyLogin = GlobalKey<FormState>();
 
-  void login() {
-    // Hide keyboard
-    // FocusScope.of(context).unfocus();
-
-    // Execute validators
-    if (!formKeyLogin.currentState!.validate())
-      return;
-    
+  Future<LoginResponse> login() async {
     // Make save in all fields
     formKeyLogin.currentState?.save();
 
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
+
+    Response response = await service.login(email, password);
+
+    // TODO: Guardar token en preferencias
+
+    return LoginResponse.fromJson(response.body);
   }
 
   void mutateHidePassword() {

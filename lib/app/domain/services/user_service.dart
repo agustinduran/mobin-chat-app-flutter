b047a58ecb88fef3c7d78d256b126e77c34d57ea
Agustín Durán <agustineduran@gmail.com>
2022-05-26 02:54:12 -0300

    Login service

--- lib/app/domain/services/user_service.dart
+++ lib/app/domain/services/user_service.dart
@@ -1,14 +1,14 @@
 import 'package:get/get.dart';
 import 'package:mobin_app/app/data/environment.dart';
-import 'package:mobin_app/app/domain/entities/user.dart';
+import 'package:mobin_app/app/domain/entities/user_response.dart';
 
 class UserService extends GetConnect {
 
   final String _urlSignUp = Environment.HOST + Environment.ENDPOINT_SIGN_UP;
+  final String _urlLogin = Environment.HOST + Environment.ENDPOINT_LOGIN;
   // final String urlUser = Environment.HOST + Environment.ENDPOINT_USER;
 
   Future<Response> signUp(User user) async {
-
     Response response = await post(
       _urlSignUp,
       user.toJson(),
@@ -16,6 +16,23 @@ class UserService extends GetConnect {
         'Content-Type': 'application/json'
       }
     );
+
+    return response;
+  }
+
+  Future<Response> login(String email, String password) async {
+    Response response = await post(
+      _urlLogin,
+      {
+        'username': email,
+        'password': password
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    );
+
+    print(response.body);
 
     return response;
   }

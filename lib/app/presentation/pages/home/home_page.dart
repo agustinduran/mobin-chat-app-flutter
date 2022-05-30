16a2a5504b19efb7dff766ae706a547494552a85
Agustín Durán <agustineduran@gmail.com>
2022-05-30 01:46:41 -0300

    create template for home page

--- lib/app/presentation/pages/home/home_page.dart
+++ lib/app/presentation/pages/home/home_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mobin_app/app/presentation/pages/home/home_controller.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomeController controller = new HomeController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text('Home'),
+      bottomNavigationBar: _createBottomNavigationBar(context),
+    );
+  }
+  
+  Widget _createBottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'home-label',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'chats-label',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'profile-label',
+        ),
+      ],
+      currentIndex: 0,
+      selectedItemColor: Colors.amber[800],
+      onTap: (index) {
+        print('index: $index');
+      },
     );
   }
 }

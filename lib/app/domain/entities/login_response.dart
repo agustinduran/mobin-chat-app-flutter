97a2208b7552fe0b3399c26fd0550ecd97383b7f
Agustín Durán <agustineduran@gmail.com>
2022-05-31 01:37:08 -0300

    add user to json normalize

--- lib/app/domain/entities/login_response.dart
+++ lib/app/domain/entities/login_response.dart
@@ -1,18 +1,23 @@
 import 'dart:convert';
 
+import '../../data/models/user.dart';
+
 LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
 
 class LoginResponse {
     LoginResponse({
         required this.success,
         this.token,
+        this.user,
     });
 
     final bool success;
     final String? token;
+    final User? user;
 
     factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
         success: json["success"],
         token: json["token"],
+        user: json["user"] == null ? null : User.fromJson(json["user"]),
     );
 }

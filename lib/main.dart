d537828650e68b34f46a53f955905e5f47132828
Agustín Durán <agustineduran@gmail.com>
2022-05-31 02:20:44 -0300

    if has user go to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:mobin_app/app/data/environment.dart';
+import 'package:mobin_app/app/data/models/user.dart';
 import 'package:mobin_app/app/data/traslation/messages_traslations.dart';
 import 'package:mobin_app/app/presentation/pages/home/home_page.dart';
 import 'package:mobin_app/app/presentation/pages/login/login_page.dart';
 import 'package:mobin_app/app/presentation/pages/sign_up/sign_up_page.dart';
 
-void main() {
+User userConnected = User.fromJson(GetStorage().read('USER_CONNECTED') ?? {});
+// String token = GetStorage().read('ACCESS_TOKEN');
+
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
       title: 'title-app'.tr,
       translations: MessageTranslations(),
       locale: Get.deviceLocale,
-      initialRoute: Environment.PATH_LOGIN_PAGE,
+      initialRoute: userConnected.id != null ? Environment.PATH_HOME: Environment.PATH_LOGIN_PAGE,
       getPages: [
         GetPage(
           name: Environment.PATH_LOGIN_PAGE,

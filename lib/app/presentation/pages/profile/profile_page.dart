7a5db284c4841c0767207a99d6338691fa317c5e
Agustín Durán <agustineduran@gmail.com>
2022-06-01 04:45:53 -0300

    profile page

--- lib/app/presentation/pages/profile/profile_page.dart
+++ lib/app/presentation/pages/profile/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mobin_app/app/data/environment.dart';
 import 'package:mobin_app/app/presentation/pages/profile/profile_controller.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -9,9 +10,65 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Profile page')
-      )
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => controller.signOut(),
+        backgroundColor: Environment.COLOR_PRIMARY,
+        child: const Icon(Icons.power_settings_new)
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            createCircleImageUser(),
+            const SizedBox(height: 20),
+            createUserInfo(
+                'profile-name'.tr,
+                '${controller.user.name ?? ''} ${controller.user.surname ?? ''}',
+                Icons.person
+            ),
+            createUserInfo(
+                'profile-email'.tr,
+                controller.user.email ?? '',
+                Icons.email
+            ),
+            createUserInfo(
+                'profile-phone'.tr,
+                controller.user.phone ?? '',
+                Icons.phone
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createUserInfo(String title, String subtitle, IconData iconData) {
+    return Container(
+      margin: const EdgeInsets.only(left: 30, right: 30),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+      ),
+    );
+  }
+
+  Widget createCircleImageUser() {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 30),
+        width: 200,
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: ClipOval(
+            child: FadeInImage.assetNetwork(
+                fit: BoxFit.cover,
+                placeholder: Environment.IMG_USER_PROFILE_PLACEHOLDER,
+                // image: controller.user.image!
+                image: 'https://www.ldsdb.com/dadmin/photosfldr/mission/8275_6V5A8958ee.jpg'
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

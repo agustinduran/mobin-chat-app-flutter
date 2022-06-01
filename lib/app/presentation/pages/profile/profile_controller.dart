ae0db82019671e6c68427dce684c234c22da4969
Agustín Durán <agustineduran@gmail.com>
2022-06-01 04:18:10 -0300

    profile controller

--- lib/app/presentation/pages/profile/profile_controller.dart
+++ lib/app/presentation/pages/profile/profile_controller.dart
@@ -1,6 +1,16 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mobin_app/app/data/environment.dart';
+import 'package:mobin_app/app/data/models/user.dart';
 
 class ProfileController extends GetxController {
 
+  User user = User.fromJson(GetStorage().read('USER_CONNECTED') ?? {});
+
+  void signOut() {
+    GetStorage().remove('USER_CONNECTED');
+    GetStorage().remove('ACCESS_TOKEN');
+    Get.offNamedUntil(Environment.PATH_LOGIN_PAGE, (route) => false);
+  }
 
 }

739eaead881b6fe6b6b744df1102359abcb9a136
Agustín Durán <agustineduran@gmail.com>
2022-06-09 01:58:50 -0300

    get users in contacts controller

--- lib/app/presentation/pages/contacts/contacts_controller.dart
+++ lib/app/presentation/pages/contacts/contacts_controller.dart
@@ -1,6 +1,19 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mobin_app/app/domain/entities/contacts_response.dart';
+import 'package:mobin_app/app/domain/services/user_service.dart';
 
 class ContactsController extends GetxController {
+
+  UserService service = UserService();
+
+  String token = GetStorage().read('ACCESS_TOKEN') ?? '';
+
+  Future<ContactsResponse> getUsers() async {
+    Response response = await service.getUsers(token);
+    ContactsResponse resp = ContactsResponse.fromJson(response.body);
+    return resp;
+  }
 
 
 }

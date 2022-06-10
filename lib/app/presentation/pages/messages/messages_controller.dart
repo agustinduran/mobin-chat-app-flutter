28d7d9d0f72356dffd7023d53cfa1d118ab26a83
Agustín Durán <agustineduran@gmail.com>
2022-06-10 00:20:38 -0300

    friend var

--- lib/app/presentation/pages/messages/messages_controller.dart
+++ lib/app/presentation/pages/messages/messages_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:mobin_app/app/data/models/user.dart';
 
 class MessagesController extends GetxController {
 
+  User friend = User.fromJson(Get.arguments['user']);
 
 }

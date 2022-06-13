044cdaa2e39347f6c24cae33118e6d89b4c48929
Agustín Durán <agustineduran@gmail.com>
2022-06-13 02:32:32 -0300

    added service for create chat to messages controller

--- lib/app/presentation/pages/messages/messages_controller.dart
+++ lib/app/presentation/pages/messages/messages_controller.dart
@@ -1,8 +1,33 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mobin_app/app/data/models/chat.dart';
 import 'package:mobin_app/app/data/models/user.dart';
+import 'package:mobin_app/app/domain/entities/general_response.dart';
+import 'package:mobin_app/app/domain/services/chat_service.dart';
 
 class MessagesController extends GetxController {
 
-  User friend = User.fromJson(Get.arguments['user']);
+  User friend  = User.fromJson(Get.arguments['user']);
+  User user    = User.fromJson(GetStorage().read('USER_CONNECTED') ?? {});
+  String token = GetStorage().read('ACCESS_TOKEN') ?? '';
+
+  ChatService service = ChatService();
+
+  MessagesController() {
+    createChat();
+  }
+
+  Future<GeneralResponse> createChat() async {
+    Chat chat = Chat(
+      idUserTransmitter: user.id,
+      idUserReceiver: friend.id
+    );
+
+    Response response = await service.create(chat, token);
+    GeneralResponse generalResponse = GeneralResponse.fromJson(response.body);
+    return generalResponse;
+  }
+
+
 
 }
